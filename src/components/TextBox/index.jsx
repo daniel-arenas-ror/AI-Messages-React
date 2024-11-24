a5f6fe6da8ae5a8414eb45a3133c897cff836012
Daniel Arenas <danielarenas@lahaus.com>
2024-11-23 19:29:11 -0500

    fix style message Box

--- src/components/TextBox/index.jsx
+++ src/components/TextBox/index.jsx
@@ -12,12 +12,6 @@ function TextBox({ threadId }) {
   const handleInputSubmit = () => {
     if(inputRef.current.value.length === 0) return;
 
-    //addMessage({
-    //  msg: inputRef.current.value,
-    //  time: getTime(),
-    //  sent: true
-    //})
-
     let newMessage = {
       message: {
         thread_id: threadId,
@@ -45,26 +39,17 @@ function TextBox({ threadId }) {
 
   const inputRef = useRef(null)
 
-  const handleEmojiClick = () => {
-
-  }
-
   return(
     <div
-      className="flex items-center bg-[#202d33] w-100 h-[70px] p-2"
+      className="flex flex-row bg-[#202d33] w-100 h-[70px] p-2"
     >
-      <RoundedBtn icon={<BiHappy onClick={handleEmojiClick} />} />
-      <span className="ml-2">
-        <RoundedBtn icon={<AiOutlinePaperClip />} />
-      </span>
-
       <input
         type="text"
-        placeholder="Type a message"
-        className="bg-[#2c3943] rounded-lg outline-none text-small text-neutral-200 w-100 h-100 px-3 placeholder:text-sm placeholder:text-[#8796a1]"
+        placeholder="Escribe tu mensaje..."
+        className="bg-[#2c3943] rounded-lg outline-none text-small text-neutral-200 w-full h-[40px] px-2 my-2 placeholder:text-sm placeholder:text-[#8796a1]"
         ref={inputRef}
       />
-      <span className="ml-2">
+      <span className="m-2">
         <RoundedBtn icon={<MdSend onClick={handleInputSubmit} />} /> :
       </span>
     </div>
